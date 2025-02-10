c5afecfd69777e862fece710d64983d006bb8659
fractal-robot <gq0x6y2e@duck.com>
2025-02-10 02:58:09 +0100

    Removed sxhkd, awesomewm, rofi. Added hyperland

--- .config/awesome/main/user-variables.lua
+++
@@ -1,9 +0,0 @@
-local _M = {
-	terminal = "kitty",
-	editor = "nvim",
-	modkey = "Mod4",
-}
-
-_M.editor_cmd = _M.terminal .. _M.editor
-
-return _M
--- .config/nvim-backup/lua/plugins/nabla.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"jbyuki/nabla.nvim",
-
-	config = function()
-		vim.keymap.set("n", "<leader>pm", function()
-			require("nabla").popup()
-		end, { desc = "[P]review [M]ath" })
-	end,
-}
