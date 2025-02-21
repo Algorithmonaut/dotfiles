8c12dd178f90e6847d2c752ff7829a96da2694be
fractal-robot <udder-t-shirt-axis@duck.com>
2025-02-21 22:55:30 +0100

    Making nvim correctly work on server

--- .config/fish/config-server.fish
+++ .config/fish/config-server.fish
@@ -2,3 +2,18 @@ if status is-interactive
     abbr --add v nvim
     abbr --add d docker compose
 end
+
+# CONF: Environment variables
+
+# C: XDG
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_STATE_HOME "$HOME/.local/state"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+
+# C: Other
+if status is-interactive
+    set -x EDITOR /bin/nvim
+    set -x MANPAGER "nvim +Man!"
+
+end
