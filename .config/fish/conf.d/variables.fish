a3e56aa12f7fff9c4bb7b34a67d7edffd4cd954d
Algorithmonaut <udder-t-shirt-axis@duck.com>
2025-03-27 15:10:38 +0100

    Removing special workspaces on hyprland

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -3,6 +3,7 @@ set -x XDG_DATA_HOME "$HOME/.local/share"
 set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x XDG_STATE_HOME "$HOME/.local/state"
 set -x XDG_CACHE_HOME "$HOME/.cache"
+set -x WINEPREFIX "$HOME/.wine"
 
 # CONF: Other variables
 set -x HYPRSHOT_DIR "$HOME/documents/screenshots/"
